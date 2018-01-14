4a2104f161c23a6679463d69abb005b262b2a30f
Micah Bell <redfroglet@gmail.com>
2018-01-13 21:01:02 -0600

    This is the first commit 01-13-2017

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+var app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+app.listen(3000)
